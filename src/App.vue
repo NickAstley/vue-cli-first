233fc6dbce4e599cbe4fed963a05957cd33442d0
NickAstley <andreanicastro1994@gmail.com>
2022-06-20 17:22:58 +0200

    Added Jumbo component

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div>
     <PageTitle></PageTitle>
+    <JumboHero></JumboHero>
   </div>
 </template>
 
 <script>
   import PageTitle from "./components/PageTitle.vue";
+  import JumboHero from "./components/JumboHero.vue";
 
   export default {
     name: 'App',
     components: {
-    PageTitle
+    PageTitle,
+    JumboHero
 }
   };
 </script>
